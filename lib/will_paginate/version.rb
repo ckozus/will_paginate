8f5b79b5462f93bbab3f10208ccb0349f0935819
Carlos Kozuszko <ckozus@gmail.com>
2015-02-10 12:05:50 -0300

    Adding a minor-minor version bump.

--- lib/will_paginate/version.rb
+++ lib/will_paginate/version.rb
@@ -3,7 +3,8 @@ module WillPaginate #:nodoc:
     MAJOR = 3
     MINOR = 0
     TINY  = 4
+    HACK  = 1
 
-    STRING = [MAJOR, MINOR, TINY].join('.')
+    STRING = [MAJOR, MINOR, TINY, HACK].join('.')
   end
 end
